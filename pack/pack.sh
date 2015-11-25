c7a473e548ff190fcd68625b2c3330088e3b02bc
Sulverus <sulverus@gmail.com>
2015-11-25 16:05:06 +0300

    CI/CD debian packages support

--- pack/pack.sh
+++ pack/pack.sh
@@ -2,6 +2,9 @@
 if [ $# -eq 3 ] ; then
     make build-$1 os=$2 dist=$3
     make export-$1 os=$2 dist=$3
+elif [ $# -eq 5 ] ; then
+    make os=$2 dist=$3 product=$4 uri=$5 build-$1
+    make os=$2 dist=$3 export-$1
 else
     echo 'build skipped'
     echo 'Usage:'
