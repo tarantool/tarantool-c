3834daeb8159b0021d68e459b62a2294efd285a0
Sulverus <sulverus@gmail.com>
2015-11-26 18:40:05 +0300

    Accept flag for apt-get

--- test.sh
+++ test.sh
@@ -13,8 +13,8 @@ echo "Preparing tests"
 curl http://tarantool.org/dist/public.key | sudo apt-key add -
 echo "deb http://tarantool.org/dist/master/ubuntu/ `lsb_release -c -s` main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
 sudo apt-get update > /dev/null
-sudo apt-get -q install tarantool tarantool-dev
-sudo apt-get -q install python-yaml
+sudo apt-get -q -y install tarantool tarantool-dev
+sudo apt-get -q -y install python-yaml
 
 cmake . -DCMAKE_BUILD_TYPE=Debug
 make
