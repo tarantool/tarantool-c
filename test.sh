688eda7b3d4994273dd27959325a2e39c752d0e7
Sulverus <sulverus@gmail.com>
2015-12-07 19:32:27 +0300

    Cloud build updated, test enabled

--- test.sh
+++ test.sh
@@ -1,15 +1,3 @@
-if [ $# -eq 0 ]; then
-    echo 'Nothing to do'
-    exit 0
-fi
-
-if [ $1 != 'none' ]; then
-    echo 'Test skipped(packaging mode)'
-    exit 0
-fi
-
-echo "Preparing tests"
-
 curl http://tarantool.org/dist/public.key | sudo apt-key add -
 echo "deb http://tarantool.org/dist/master/ubuntu/ `lsb_release -c -s` main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
 sudo apt-get update > /dev/null
