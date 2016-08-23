bd99af2e82b687b89c572b6e41c6afcf00cb7d87
bigbes <bigbes@gmail.com>
2016-08-23 14:35:32 +0400

    Update conf.py
    update version to be consistent

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -58,9 +58,9 @@ author = u'Eugine Blikh'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.0'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.2.0'
+release = '1.0.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
