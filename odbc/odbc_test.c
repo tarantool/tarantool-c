0e0f1d3fa660a4c4507562d5fda0f254a1cd0b08
alexey.gadzhiev <alexey.gadzhiev@corp.mail.ru>
2018-05-14 20:09:26 +0300

    Test and working row_count and input parameters

--- odbc/odbc_test.c
+++ odbc/odbc_test.c
@@ -51,7 +51,7 @@ test_connect(const char *dsn) {
 				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
 
 				// Connect to data source
-				retcode = SQLConnect(hdbc, dsn, SQL_NTS,
+				retcode = SQLConnect(hdbc, (SQLCHAR *)dsn, SQL_NTS,
 						     (SQLCHAR*) NULL, 0, NULL, 0);
 
 				// Allocate statement handle
@@ -101,10 +101,10 @@ test_driver_connect(const char *dsn) {
 				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
 
 				char out_dsn[1024];
-				int out_len=0;
+				short out_len=0;
 				// Connect to data source
-				retcode = SQLDriverConnect(hdbc, 0, dsn, SQL_NTS, out_dsn, sizeof(out_dsn),
-							   &out_len, SQL_DRIVER_NOPROMPT);
+				retcode = SQLDriverConnect(hdbc, 0, (SQLCHAR *)dsn, SQL_NTS, (SQLCHAR *)out_dsn,
+							   sizeof(out_dsn), &out_len, SQL_DRIVER_NOPROMPT);
 				fprintf(stderr,"OUTDSN|%s|\n",out_dsn);
 
 
@@ -156,10 +156,10 @@ test_prepare(const char *dsn, const char *sql) {
 				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
 
 				char out_dsn[1024];
-				int out_len=0;
+				short out_len=0;
 				// Connect to data source
-				retcode = SQLDriverConnect(hdbc, 0, dsn, SQL_NTS, out_dsn, sizeof(out_dsn),
-							   &out_len, SQL_DRIVER_NOPROMPT);
+				retcode = SQLDriverConnect(hdbc, 0,(SQLCHAR *)dsn, SQL_NTS, (SQLCHAR *)out_dsn,
+							   sizeof(out_dsn), &out_len, SQL_DRIVER_NOPROMPT);
 
 
 				// Allocate statement handle
@@ -214,10 +214,10 @@ test_execdirect(const char *dsn, const char *sql) {
 				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
 
 				char out_dsn[1024];
-				int out_len=0;
+				short out_len=0;
 				// Connect to data source
-				retcode = SQLDriverConnect(hdbc, 0, dsn, SQL_NTS, out_dsn, sizeof(out_dsn),
-							   &out_len, SQL_DRIVER_NOPROMPT);
+				retcode = SQLDriverConnect(hdbc, 0, (SQLCHAR *)dsn, SQL_NTS, (SQLCHAR *)out_dsn,
+							   sizeof(out_dsn), &out_len, SQL_DRIVER_NOPROMPT);
 
 
 				// Allocate statement handle
@@ -228,6 +228,139 @@ test_execdirect(const char *dsn, const char *sql) {
 						retcode = SQLExecDirect(hstmt, (SQLCHAR*)sql, SQL_NTS);
 						if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
 							ret_code = 1;
+						} else {
+							show_error(SQL_HANDLE_STMT, hstmt);
+							ret_code = 0;
+						}
+						SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+					}
+					SQLDisconnect(hdbc);
+				} else {
+					show_error(SQL_HANDLE_DBC, hdbc);
+				}
+				SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+			}
+		}
+		SQLFreeHandle(SQL_HANDLE_ENV, henv);
+	}
+	return ret_code;
+}
+
+int
+test_execrowcount(const char *dsn, const char *sql,int val) {
+	int ret_code = 0;
+	SQLHENV henv;
+	SQLHDBC hdbc;
+	SQLHSTMT hstmt;
+	SQLRETURN retcode;
+
+	SQLCHAR * OutConnStr = (SQLCHAR * )malloc(BUFSZ);
+	SQLSMALLINT * OutConnStrLen = (SQLSMALLINT *)malloc(BUFSZ);
+
+	// Allocate environment handle
+	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+	// Set the ODBC version environment attribute
+	if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+		retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void*)SQL_OV_ODBC3, 0);
+
+		// Allocate connection handle
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+			// Set login timeout to 5 seconds
+			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+				char out_dsn[1024];
+				short out_len=0;
+				// Connect to data source
+				retcode = SQLDriverConnect(hdbc, 0,(SQLCHAR *) dsn, SQL_NTS,(SQLCHAR *)out_dsn,
+							   sizeof(out_dsn), &out_len, SQL_DRIVER_NOPROMPT);
+
+
+				// Allocate statement handle
+				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+					retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+					// Process data
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						retcode = SQLExecDirect(hstmt, (SQLCHAR*)sql, SQL_NTS);
+						SQLLEN ar = -1;
+						if (retcode == SQL_SUCCESS  &&
+						    ((retcode=SQLRowCount(hstmt,&ar)) == SQL_SUCCESS)) {
+							if (ar == val) 							 
+								ret_code = 1;
+							else {
+								fprintf(stderr,"Affected row = %ld expected %d\n",ar,val);
+								ret_code = 0;
+							}
+						} else {
+							show_error(SQL_HANDLE_STMT, hstmt);
+							ret_code = 0;
+						}
+						SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+					}
+					SQLDisconnect(hdbc);
+				} else {
+					show_error(SQL_HANDLE_DBC, hdbc);
+				}
+				SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+			}
+		}
+		SQLFreeHandle(SQL_HANDLE_ENV, henv);
+	}
+	return ret_code;
+}
+
+int
+test_inbind(const char *dsn, const char *sql,int p1,const char *p2) {
+	int ret_code = 0;
+	SQLHENV henv;
+	SQLHDBC hdbc;
+	SQLHSTMT hstmt;
+	SQLRETURN retcode;
+
+	SQLCHAR * OutConnStr = (SQLCHAR * )malloc(BUFSZ);
+	SQLSMALLINT * OutConnStrLen = (SQLSMALLINT *)malloc(BUFSZ);
+
+	// Allocate environment handle
+	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+	// Set the ODBC version environment attribute
+	if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+		retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void*)SQL_OV_ODBC3, 0);
+
+		// Allocate connection handle
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+			// Set login timeout to 5 seconds
+			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+				char out_dsn[1024];
+				short out_len=0;
+				// Connect to data source
+				retcode = SQLDriverConnect(hdbc, 0,(SQLCHAR *) dsn, SQL_NTS,(SQLCHAR *)out_dsn,
+							   sizeof(out_dsn), &out_len, SQL_DRIVER_NOPROMPT);
+
+
+				// Allocate statement handle
+				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+					retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+					retcode = SQLPrepare(hstmt,(SQLCHAR*)sql, SQL_NTS);
+					retcode = SQLBindParameter(hstmt, 1, SQL_PARAM_INPUT,
+								   SQL_C_INTEGER, SQL_INTEGER, 0, 0, &int_val, 0, 0); 
+					retcode = SQLBindParameter(hstmt, 2, SQL_PARAM_INPUT,
+								   SQL_C_CHAR, SQL_CHAR, 0, 0, &str_val, SQL_NTS, 0); 
+
+					// Process data
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						retcode = SQLExecute(hstmt);
+						SQLLEN ar = -1;
+						if (retcode == SQL_SUCCESS) {
+								ret_code = 1;
+							else {
+								ret_code = 0;
+							}
 						} else {
 							show_error(SQL_HANDLE_STMT, hstmt);
 							ret_code = 0;
@@ -283,4 +416,24 @@ main(int ac, char* av[])
 	testfail(test_execdirect(good_dsn,"wrong wrong wrong * from test"));
 	test(test_execdirect(good_dsn,"create table t (id string, primary key(id))"));
 	test(test_execdirect(good_dsn,"drop table t"));
+
+	testfail(test_execdirect(good_dsn,"drop table str_table"));
+	fprintf(stderr,"SQLRowCount section: next 2 ok\n");
+	test(test_execrowcount(good_dsn,"CREATE TABLE str_table(id STRING, val INTEGER,PRIMARY KEY (val))",1));
+	test(test_execrowcount(good_dsn,"drop table str_table",1));
+
+	fprintf(stderr,"SQLRowCount next is fail: insert into absent table.\n");
+	testfail(test_execrowcount(good_dsn,"INSERT INTO str_table(id,val) VALUES ('aa',1)",0));
+
+	fprintf(stderr,"SQLRowCount next is ok: create table.\n");
+	test(test_execrowcount(good_dsn,"CREATE TABLE str_table(id STRING, val INTEGER,PRIMARY KEY (val))",1));
+	fprintf(stderr,"SQLRowCount next is ok: insert.\n");
+	test(test_execrowcount(good_dsn,"INSERT INTO str_table(id,val) VALUES ('aa',1)",1));
+	fprintf(stderr,"SQLRowCount next is fail: duplicate insert.\n");
+	testfail(test_execrowcount(good_dsn,"INSERT INTO str_table(id,val) VALUES ('aa',1)",0));
+	fprintf(stderr,"SQLRowCount next 2 is ok: insert.\n");
+	test(test_execrowcount(good_dsn,"INSERT INTO str_table(id,val) VALUES ('bb',2)",1));
+//	test(test_execrowcount(good_dsn,"drop table str_table",1));
+	
+	
 }
