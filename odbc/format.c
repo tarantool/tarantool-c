2ecca4eef79f6f5d11d413c428e213df7d22f382
alexey.gadzhiev <alexey.gadzhiev@corp.mail.ru>
2018-07-04 19:55:56 +0300

    space format reading

---
+++ odbc/format.c
@@ -0,0 +1,75 @@
+#include <string>
+
+enum value_type {
+	STRING,
+	NUMBER,
+	OBJECT,
+	ARRAY,
+	BOOL,
+	NIL
+};
+
+struct column_def {
+	int id;
+	int type;
+	int is_nullable;
+	char *name;
+	int index_type;
+};
+
+struct format {
+	uint32_t count;
+	struct column_def *items;
+};
+
+int
+read_pair(const char** data, struct column_def *col, const char* key, size_t len)
+{
+
+}
+
+int
+read_column_def(const char** data, struct column_def *col, int num)
+{
+	col->id = num;
+	if (mp_typeof(**data) != MP_MAP)
+		return FAIL;
+	int count = mp_decode_map(data);
+	for (int i = 0; i < count; ++i) {
+		if (mp_typeof(**data) != MP_STR)
+			return FAIL;
+		size_t key_len;
+		const char *key = mp_decode_str(data, &key_len);
+		if (read_pair(data, col, key, key_len) == FAIL)
+			return FAIL;
+	}
+	return OK;
+}
+
+int
+read_format(const char* data, struct format *root)
+{
+	if (mp_typeof(*data) != MP_ARRAY)
+		return FAIL;
+	root->count = mp_decode_array(&data);
+
+	if (root->count == 0)
+		return OK;
+	format->items = (struct format*) malloc(sizeof(struct type_map*)*root->count);
+	if (!format->items)
+		return FAIL;
+	for(int i = 0; i < field_count ; ++i)
+		format->items[i] = 0;
+
+	for(int i = 0; i < field_count ; ++i) {
+		format->items[i] = (struct column_def*) malloc(sizeof(struct column_def));
+		if (!format->items[i])
+			goto error;
+		if (read_column_def(&data, format->items[i], i) == FAIL)
+			goto error;
+	}
+	return OK;
+error:
+	free_format(root);
+	return FAIL;
+}
